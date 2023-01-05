c883084e467b53ec64eb66249fb7aebd9ac33144
Hoang Truong <73890707+hoangankin2211@users.noreply.github.com>
2023-01-05 14:41:08 +0700

    Update mail_transport.js

--- middlewares/mail_transport.js
+++ middlewares/mail_transport.js
@@ -11,7 +11,7 @@ const { google } = require('googleapis')
 const CLIENT_ID = '51863514190-l45u9ulnsrv2de53pcmosf9ao4nnaumh.apps.googleusercontent.com'
 const CLIENT_SECRET = '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'
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-const REFRESH_TOKEN = '1//04qVIP47oJf5NCgYIARAAGAQSNwF-L9IrIJlKnnweDX2jFShK5osoNWdAAxZZMuKFLyoopcXchBNgSDmoSCgeDx7-AAVVyAySt9E'
+const REFRESH_TOKEN = '1//04beIBH0Q6-vwCgYIARAAGAQSNwF-L9IrPCXgqCn-UocCbhTrjsF7mBrvfE_5tWDBfYRb5yfhKxm3exnLqwQrMQl3zU7l1_sMrk0'
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
